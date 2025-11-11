98493a2f55cb0ea0a83545b37132ae6c3194b6f5
Your Name <your.email@example.com>
2025-11-11 12:51:11 +0530

    container config done

---
+++ script.js
@@ -0,0 +1,9 @@
+const resumeProject = document.getElementById("resumeProject").addEventListener("click", function() {
+  window.open("https://resume-builder-project-ten.vercel.app", "_blank");
+});
+resumeProject()
+
+const ghost_animation = document.getElementById("ghost-animations").addEventListener("click", function() {
+  window.open("https://codepen.io/megh-bari/full/MWdNZdB", "_blank");
+});
+ghost_animation()
